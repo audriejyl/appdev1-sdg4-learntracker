4ce31d85660e002c56bf5f748bb227514cab4ae4
galigo_a <galigo_a@ca01.ub.lan>
2026-04-23 11:37:32 +0800

    Fixed components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,11 @@
 import { Routes } from '@angular/router';
+
 import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
   { path: 'dashboard', component: DashboardComponent },
 ];
